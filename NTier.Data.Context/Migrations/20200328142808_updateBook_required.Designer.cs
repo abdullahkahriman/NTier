2b317a292bd7f90e42ce359b7ef911e3db347b79
Abdullah Kahriman <kahriman.abdullah@gmail.com>
2020-03-28 17:29:48 +0300

    The book table mandatory fields were determined and character limit was introduced.

--- NTier.Data.Context/Migrations/20200328142808_updateBook_required.Designer.cs
+++ NTier.Data.Context/Migrations/20200328142808_updateBook_required.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NTier.Data.Context.Context;
 
 namespace NTier.Data.Context.Migrations
 {
     [DbContext(typeof(NTierDBContext))]
-    partial class NTierDBContextModelSnapshot : ModelSnapshot
+    [Migration("20200328142808_updateBook_required")]
+    partial class updateBook_required
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- NTier.Data.Model/Entity/Book.cs
+++ NTier.Data.Model/Entity/Book.cs
@@ -1,13 +1,16 @@
 ﻿using NTier.Core;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace NTier.Data.Model.Entity
 {
     public class Book : Table
     {
+        [Required]
         public string Title { get; set; }
+        [StringLength(250)]
         public string ShortDescription { get; set; }
         public string Description { get; set; }
     }
